8573ba19d17f7a202a691d502075d7d26b5c0353
sampath-ops <sampathkaali002@gmail.com>
2024-05-18 14:10:27 +0530

    Oauth Project initialized

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,15 @@
+const LoginForm = () => {
+    return (
+        <form>
+            <button className="bg-pink-400 text-white p-1 rounded-md m-1 text-lg" type="submit" name="action" value="google">
+                Sign In With Google
+            </button>
+
+            <button className="bg-black text-white p-1 rounded-md m-1 text-lg" type="submit" name="action" value="github">
+                Sign In With GitHub
+            </button>
+        </form>
+    );
+};
+
+export default LoginForm;
