ec980680e55cbcbe4ff06b673f88a58a22374e2a
sampath-ops <sampathkaali002@gmail.com>
2024-05-18 14:52:47 +0530

    exported required items from provider

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-NextAuth({
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+} = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
